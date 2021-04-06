9bea65feae0a3291bdccc4a0117d4c16eb92ecec
Rodolfo Roman <rromanv@gmail.com>
2021-04-06 18:17:47 -0500

    Agregar Nombre en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Hola {{ nombre }}</h1>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+const nombre = ref('Rodolfo Roman')
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
